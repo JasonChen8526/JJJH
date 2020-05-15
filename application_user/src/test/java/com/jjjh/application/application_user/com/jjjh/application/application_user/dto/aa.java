d0813d75ac378bdd0adff85e37c5186eb589d491
chenxinmin <752404662@qq.com>
2020-05-15 14:27:12 +0800

    111

---
+++ application_user/src/test/java/com/jjjh/application/application_user/com/jjjh/application/application_user/dto/aa.java
@@ -0,0 +1,10 @@
+package com.jjjh.application.application_user.com.jjjh.application.application_user.dto;/*
+ * @description:
+ *
+ * @author: Chen Xinmin
+ *
+ * @create: 2020-05-15 14:26
+ **/
+
+public class aa {
+}
