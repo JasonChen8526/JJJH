7c94edcbf87cdf94e099c86515694c4f28d55962
chenxinmin <752404662@qq.com>
2020-05-18 18:59:33 +0800

    添加dto

---
+++ jjjh_dto/dto_question/src/main/java/com/jjjh/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.jjjh.dto;/*
+ * @description:
+ *
+ * @author: Chen Xinmin
+ *
+ * @create: 2020-05-18 13:42
+ **/
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Integer id;
+}
